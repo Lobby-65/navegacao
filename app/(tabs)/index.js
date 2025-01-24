0c6f972f4e7064ce48f8f3eb3769f89e42ea1b4b
Robert Cauan Alves De Oliveira <150349416+Lobby-65@users.noreply.github.com>
2025-01-24 10:40:48 -0300

    Update index.js

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -46,3 +46,84 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
+
+
+
+
+
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const getUsersFromAPI = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+      setUsers(data); // Atualiza o estado com a lista de usuários
+    } catch (error) {
+      setUsers([]);
+      alert('Falha ao acessar servidor. Tente novamente mais tarde!');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getUsersFromAPI();
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.name}>{item.name}</Text>
+      <Text style={styles.email}>{item.email}</Text>
+      <Text style={styles.phone}>Phone: {item.phone}</Text>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <FlatList
+          data={users}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={renderItem}
+        />
+      )}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  item: {
+    padding: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    width: '100%',
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  email: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  phone: {
+    fontSize: 14,
+    color: 'blue',
+  },
+});
