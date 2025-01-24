c438c1b068d02a8a50b41b9b2d6f5133ba1eca88
Robert Cauan Alves De Oliveira <150349416+Lobby-65@users.noreply.github.com>
2025-01-24 11:19:35 -0300

    Add files via upload

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,19 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [posts, setPosts] = useState([]); 
+  const [users, setUsers] = useState([]);
   const [isLoading, setLoading] = useState(true);
 
-  const getPostsNaAPI = async () => {
+  const getUsersFromAPI = async () => {
     try {
       setLoading(true);
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos/2');
-      const post = await response.json();
-      setPosts([post]); 
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+      setUsers(data); // Atualiza o estado com a lista de usuários
     } catch (error) {
-      setPosts([]);
+      setUsers([]);
       alert('Falha ao acessar servidor. Tente novamente mais tarde!');
     } finally {
       setLoading(false);
@@ -21,20 +21,33 @@ export default function App() {
   };
 
   useEffect(() => {
-    getPostsNaAPI();
+    getUsersFromAPI();
   }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.name}>{item.name}</Text>
+      <Text style={styles.phone}>Phone: {item.phone}</Text>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
       {isLoading ? (
-        <ActivityIndicator />
+        <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        <Text style={{ fontSize: 20 }}>
-          {posts.length > 0 ? `Título: ${posts[0].title}` : 'Não possui postagens carregadas'}
-        </Text>
+        <FlatList
+          data={users}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={renderItem}
+        />
+
+        
       )}
       <StatusBar style="auto" />
     </View>
+
+    
   );
 }
 
@@ -42,8 +55,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
   },
-});
-
+  item: {
+    padding: 1,
+    borderBottomWidth: 4,
+    borderBottomColor: 'black',
+    width: '100%',
+    }
+  });
